dc8068ea80fa44edff4ff8b07d62bc3d918b9a1e
Marco Velasquez Figarella <marcovelasquez90@hotmail.com>
2022-04-26 17:38:44 -0400

    diseño de escritorio

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 const overlay = document.querySelector(".overlay");
 const nav = document.querySelector("nav");
-// const menu = document.querySelector(".menu");
 
 function openMenu(e) {
     e.querySelector(".open-menu").classList.toggle("openMenuActive");
     e.querySelector(".close-menu").classList.toggle("closeMenuActive");
-    // menu.querySelector(".open-menu").classList.toggle("openMenuActive");
-    // menu.querySelector(".close-menu").classList.toggle("closeMenuActive");
     nav.classList.toggle("navOpen");
     overlay.classList.toggle("overlayOpen");
 }
